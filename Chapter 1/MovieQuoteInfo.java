38858c4f745f02ba5e0f784602164c3d4d6c41e3
seeigi <justinenavaja82@gmail.com>
2024-08-14 21:56:33 +0800

    Create  MovieQuoteInfo.java

---
+++ Chapter 1/MovieQuoteInfo.java
@@ -0,0 +1,16 @@
+public class MovieQuoteInfo {
+
+  public static void main(String[] args) {
+   
+    String quote = "Every breath I take without your permission raises my self-esteem.";
+
+    String movie = "Rick and Morty";
+    String character = "Rick";
+    String releaseDate = "December 6, 2019";
+    
+    System.out.println("Quote: " + quote);
+    System.out.println("Movie: " + movie);
+    System.out.println("Character: " + character);
+    System.out.println("Release Date: " + releaseDate);
+  }
+}
