ce052a2f2fa6db2567da82ac8886c4cc7ca372c5
Cj-Navaja <justinenavaja82@gmail.com>
2024-08-21 22:27:57 +0800

    Create Testing Gooeys with JOptionPane's showMessageDialog.java

---
+++ Chapter 1/Testing Gooeys with JOptionPane's showMessageDialog.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class SwingAlert {
+
+  public static void main(String[] args) {
+    // Message to display
+    String message = "Hello World!";
+
+    // Display the message dialog
+    JOptionPane.showMessageDialog(null, message);
+  }
+}
