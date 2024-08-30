45f15486aee9a16be2430b92b7cd899cde6bf9c6
Cj-Navaja <justinenavaja82@gmail.com>
2024-08-30 08:09:48 +0700

    warmup_sleepin.java

--- Chapter 1/warmup_sleepin.java
+++ Chapter 1/warmup_sleepin.java
@@ -1,11 +1,23 @@
-public class warmup_sleepin {
-    public static boolean sleepIn(boolean weekday, boolean vacation) {
-        return !weekday || vacation;
-    }
+import java.util.Scanner;
 
+public class WeekendDreams {
     public static void main(String[] args) {
-        System.out.println(sleepIn(false, false));
-        System.out.println(sleepIn(true, false));
-        System.out.println(sleepIn(false, true));
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("Is it Saturday or Sunday? (true/false): ");
+        boolean isWeekend = scanner.nextBoolean();
+
+      
+        System.out.print("Are you on vacation? (true/false): ");
+        boolean onVacation = scanner.nextBoolean();
+
+       
+        boolean canSleepIn = isWeekend || onVacation;
+
+       
+        System.out.println(canSleepIn ? "Sweet dreams! You can sleep in." : "Time to rise and shine!");
+
+        scanner.close();
     }
 }
