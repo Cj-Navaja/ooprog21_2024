b5cc6ef7395dc7a47283b1f54f9f97aa49dbf086
Cj-Navaja <justinenavaja82@gmail.com>
2024-09-21 11:20:04 +0800

    FirstAddressExercise.java

---
+++ Chapter 3/FirstAddressExercise.java
@@ -0,0 +1,12 @@
+public class FirstAddressExercise {
+    public static void main(String[] args) {
+        System.out.println("First Java application");
+        displayAddress();
+    }
+
+    public static void displayAddress() {
+        System.out.println("Huawei Technologies Co., Ltd.");
+        System.out.println("Shenzhen, Guangdong, China");
+        System.out.println("Company: Huawei Technologies Co., Ltd.");
+    }
+}
